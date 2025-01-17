231047e5bd8250bd24b79ebfcb87a2037b695af0
sahanur0k <sahanuralam0k@gmail.com>
2025-01-17 22:33:00 +0530

    Update 11_ Write a C program to find if roll number 20 is present or not.c

--- DAY-2/11_ Write a C program to find if roll number 20 is present or not.c
+++ DAY-2/11_ Write a C program to find if roll number 20 is present or not.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int n, rollNumber, found = 0;
-    printf("Enter the number of students: ");
+    int n, roll, found = 0;
+
+    printf("How many students? ");
     scanf("%d", &n);
 
-    printf("Enter the roll numbers:\n");
+    printf("Enter roll numbers:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &rollNumber);
-        if (rollNumber == 20) {
+        scanf("%d", &roll);
+        if (roll == 20) {
             found = 1;
         }
     }
 
     if (found) {
-        printf("Roll number 20 is present.\n");
+        printf("Roll number 20 is here.\n");
     } else {
-        printf("Roll number 20 is not present.\n");
+        printf("Roll number 20 is not here.\n");
     }
 
     return 0;
