da7221a71cd722070310b2839f501bd3c754d839
sahanur0k <sahanuralam0k@gmail.com>
2025-01-17 22:00:38 +0530

    Update 3_ Swap 2 numbers without using add, minus operators, third variable, and pointers.c

--- DAY-1/3_ Swap 2 numbers without using add, minus operators, third variable, and pointers.c
+++ DAY-1/3_ Swap 2 numbers without using add, minus operators, third variable, and pointers.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    int a = 5, b = 10;
+    a = a * b; // Step 1
+    b = a / b; // Step 2
+    a = a / b; // Step 3
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
